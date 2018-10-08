716b9bbf0d80e9823c2f194006ae397b0dc42b52
Min <Min@Mins-MacBook-12.local>
2018-10-08 20:56:13 +0800

    更新 LaunchBar 版权

--- LaunchBar/Rime Dic Capture.lbaction/Contents/Scripts/default.sh
+++ LaunchBar/Rime Dic Capture.lbaction/Contents/Scripts/default.sh
@@ -7,6 +7,6 @@ cd /Users/apple/Library/Rime
 echo "$# arguments passed"
 for ARG in "$@"; do
     LOWERCASE=$(echo $ARG | tr '[A-Z]' '[a-z]')
-    echo "$ARG $LOWERCASE" >> luna_pinyin.extended.dict.yaml
+    echo "$ARG\t$LOWERCASE" >> luna_pinyin.extended.dict.yaml
 done
 
